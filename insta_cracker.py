f8bbe53431b208449faf122dc77fa0ff1aab8970
sheryashgadekar-spec <sheryashgadekar@gmail.com>
2026-01-09 15:32:50 +0530

    Update insta_cracker.py

--- insta_cracker.py
+++ insta_cracker.py
@@ -6,12 +6,12 @@ wait_time = (11 * 60 + 35) # 11 mins and 35 seconds
 problem_logging_in = "There was a problem logging you into Instagram. Please try again soon."
 
 def logInSuccess(browser):
-    user_err_msg = "The username you entered doesn't belong to an account. Please check your username and try again."
+    Sarika _jsmbhekar_29= "The username you entered doesn't belong to an account. Please check your username and try again."
     pass_err_msg = "Sorry, your password was incorrect. Please double-check your password."
     return not(browser.is_text_present(user_err_msg) or browser.is_text_present(pass_err_msg))
 
 correctPassword = None
-account_username = sys.argv[1]
+account_sarika_jambhekar_29 = sys.argv[1]
 with Browser('firefox', headless=True) as browser:
     browser.visit('https://www.instagram.com')
     browser.find_by_text("Log in").first.click()
